c3007ebfc382f9f79486884026d27ab9632e9782
MothWaves <mothwaves@outlook.com>
2025-03-04 02:58:22 +0000

    Add some error-checking to run.sh
    This doesn't really help because if you have virtual-env (which you need to run run.sh anyway) you'll automatically have python but I didn't realize that until I had already changed the script so it's getting added in now.

--- run.sh
+++ run.sh
@@ -3,12 +3,23 @@
 # This script is made around my own private setup for this program and will not work without changes.
 # You can use it as a starting off point, though.
 
+
+
 # Activates python virtual environment
 source .virtual-env/bin/activate
 # Sets env variables.
 source ./set-env-variables.sh
 # Runs program
-python spotify-playlists-downloader.py
+if hash python3 2>/dev/null
+then
+    python3 spotify-playlists-downloader.py  
+elif hash python 2>/dev/null
+then
+    python spotify-playlists-downloader.py
+else 
+    echo "Python not found!"
+    exit 1
+fi
 # Moves to export folder
 cd ./spotify-playlists
 # Git commit
